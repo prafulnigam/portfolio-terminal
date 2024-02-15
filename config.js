3e883eb8cac73cdaeea69de939af813dcad7d82a
Praful Nigam <prafulnigam16@gmail.com>
2024-02-16 00:51:42 +0530

    1st Upload

---
+++ config.js
@@ -0,0 +1,174 @@
+
+const config = {
+
+    "help": [
+        {
+            "title": "help",
+            "description": "for a list of commands(add flags '-d' for commands description)",
+            "info": ["help or ls command gives the list of commands","add '-d' flag for description of all commands also","write command after help to get info about that specific command like 'help typing'"]
+        },
+        {
+            "title": "clear",
+            "description": "to clear the terminal",
+            "info": ["clear or cls command clears the terminal"]
+        },
+        {
+            "title": "about",
+            "description": "to learn more about me",
+            "info": ["use about command to learn more about me"]
+        },
+        {
+            "title": "social",
+            "description": "to see my social links (add flags '-l' for links and '-d' for detailed results)",
+            "info": ["use social command to see my social links","add '-l' flag for links and '-d' flag for detailed results"]
+        },
+        {
+            "title": "projects",
+            "description": "to see my projects",
+            "info": ["use projects command to see my projects"]
+        },
+        {
+            "title": "contact",
+            "description": "to enquire about my services",
+            "info": ["use enquire command to enquire about my services"]
+        },
+        {
+            "title": "cheer",
+            "description": "to appreciate my work",
+            "info": ["use cheer command to appreciate my work"]
+        },
+        {
+            "title": "ipconfig",
+            "description": "to see your IP details",
+            "info": ["use ipconfig command to see your IP details"]
+        },
+        {
+            "title": "github",
+            "description": "to see my github stats",
+            "info": ["use github command to see my github stats"]
+        },
+        // {
+        //     "title": "download",
+        //     "description": "to download my pdf resume",
+        //     "info": ["use download command to download my pdf resume"]
+        // },
+        {
+            "title": "calc",
+            "description": "to evaluate an expression, for eg: (2 + 3)",
+            "info": ["use calc command to evaluate an expression","for eg: write 'calc 2+6*5/3'"]
+        },
+        {
+            "title": "experience",
+            "description": "to see my work experience",
+            "info": ["use experience command to see my work experience"]
+        },
+        {
+            "title": "history",
+            "description": "shows the last 10 valid commands performed, use --clear flag to clear the history",
+            "info": ["use history command to show your last 10 commands history","use --clear flag to clear the history","use history {id} command to run command of that id in your history"]
+        },
+        {
+            "title": "skills",
+            "description": "to see my skills",
+            "info": ["use skills command to see my skills"]
+        },
+        {
+            "title": "reset",
+            "description": "to reload site",
+            "info": ["use reset command to to reload site"]
+        }
+    ],
+    "terminal": {
+        "user": "$prafulnigam",
+        "host": "sudo",
+        "path": "~/guest"
+    },
+    "cheer": {
+        "responseArray": [
+            "Thank you! It makes my day😊😊😊",
+            "It is great to hear that way!😁😁😁",
+            "I would love to take credit😂😂😂",
+            "That's so good to hear! I'm glad😍😍😍"
+        ]
+    },
+    "about": "🎓 Aspiring software engineer with strong foundation in computer science and engineering, gained through BE degree program and hands-on experience with various technologies. (Graduating July 2025) \n 🌇 Proficient in MERN stack, data structures and algorithms, SQL, and DevOps. Demonstrated expertise in these technologies through successful projects and 700+ questions on DSA. \n 💻 Constantly seeking opportunities to grow and improve skills, with long-term goal of becoming a software development engineer.",
+    "social": [
+        {
+            "title": "Github",
+            "link": "https://github.com/prafulnigam"
+        },
+        {
+            "title": "LinkedIn",
+            "link": "https://www.linkedin.com/in/prafulnigam",
+            "connections": "500+"
+        },
+        {
+            "title": "LeetCode",
+            "link": "https://leetcode.com/praful"
+        },
+        {
+            "title": "GeeksForGeeks",
+            "link": "https://auth.geeksforgeeks.org/user/prafulnigam",
+            "score": "1200",
+            "rank": "100 above"
+        }
+    ],
+    "projects": [
+        {
+            "title": "Reddit Clone",
+            "link": "https://github.com/prafulnigam/Reddit-Clone",
+            "description": "The React Clone project replicates React's core functionality using HTML, CSS, and JavaScript. It showcases component-based architecture, state management, props, and event handling, enhancing understanding of frontend development concepts."
+        },
+        {
+            "title": "Railway Management System",
+            "link": "https://github.com/prafulnigam/Railway-Management-System",
+            "description": "The Railway System Management System, developed using Python, incorporates file system and database management. It efficiently manages train schedules, ticket reservations, passenger data, and route planning, offering a comprehensive solution for railway operations."
+        },
+        {
+            "title": "TextUtils",
+            "link": "https://github.com/prafulnigam/TextUtils",
+            "description": "The React text editor is equipped with essential features such as text capitalization, conversion to lowercase, copy functionality, clearing text, inserting spaces, and word count, enhancing text manipulation and editing capabilities for users."
+        },
+        {
+            "title": "Spotify Clone",
+            "link": "https://github.com/prafulnigam/Spotify-Clone",
+            "description": "The Spotify clone seamlessly integrates React for the frontend, Node.js and Express for the backend, and MongoDB for database management. It replicates Spotify's core functionalities, allowing users to browse music, create playlists, and enjoy a personalized listening experience."
+        },
+        {
+            "title": "SuperNicks",
+            "link": "https://github.com/prafulnigam/SuperNicks",
+            "description": "The shoes eCommerce website is crafted using HTML, CSS, and JavaScript. It showcases a wide array of shoes, allowing users to browse, select, and purchase their desired footwear. With a user-friendly interface and responsive design, it offers a seamless shopping experience."
+        }
+    ],
+    "contact": {
+        "email": "prafulnigam16@gmail.com"
+    },
+    "experience": [
+        {
+            "title": "Researcher at YMM",
+            "description": "My internship at YMM involved Skyy Rider Institutions & revolved around research and data analysis over certain subjects & social media. This experience equipped me with strong research skills and the ability to draw meaningful insights from data.I worked on research projects that were aimed at improving institutional processes, website & online presence. This internship provided me with a foundation in experience with internships & work-culture."
+        },
+        {
+            "title": "Operations & Shopify Development at Gemseason",
+            "description": "During my internship at GemSeason, I had the opportunity to work in two distinct roles. In the operations department, I gained valuable insights into social media, inventory control, and order processing. I helped optimize workflows and streamline processes to enhance operational efficiency.In the Shopify Development role, I focused on enhancing the company's online presence. I worked on customizing and maintaining the Shopify website, implementing new features, and improving the user experience aligning with the web developer. This experience allowed me to gain practical skills in e-commerce and web development."
+        }
+    ],
+    "skills": [
+        {
+            "title": "Technical skills are: ",
+            "description": "C++, C, MySQL, Linux, ReactJS, NodeJS, MongoDB, JSON, DSA, Docker, ExpressJS, Redux, Material UI, Bootstrap, Tailwind, Java, Javascript"
+        },
+        {
+            "title": "Other Skills are:",
+            "description": "Problem Solving, Team Leadership, Community Management, Team Building"
+        }
+    ],
+    "neofetch": {
+        "name": "Praful Nigam",
+        "title": "MERN Developer",
+        "skills": "Frontend, Backend",
+        "shell": "zsh",
+        "languages": "Javascript, C++, HTML/CSS, SQL"
+    },
+}
+export default config
